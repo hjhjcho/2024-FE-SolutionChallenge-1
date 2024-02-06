61fbe58932637ec88d54221d0f104f628944cc5b
sohyun127 <so405@naver.com>
2024-02-06 13:43:23 +0900

    [UI] : navigation 구현 / #5

--- FE-ANDROID/feature/src/main/java/com/teamfairy/feature/MainActivity.kt
+++ FE-ANDROID/feature/src/main/java/com/teamfairy/feature/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.teamfairy.feature
 
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.teamfairy.core_ui.base.BindingActivity
 import com.teamfairy.feature.databinding.ActivityMainBinding
 
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun initView() {
+        initBottomNavigation()
+    }
 
+    private fun initBottomNavigation() {
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.fc_main)?.findNavController()!!
+
+        with(binding) {
+            navController.let { NavController ->
+                botNavMain.setupWithNavController(NavController)
+            }
+        }
     }
 }
